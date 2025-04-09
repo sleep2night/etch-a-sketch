17740fe6ba0a924dad2c8d04722349c79120b943
Roy Park <roydp@uci.edu>
2025-04-09 14:08:39 -0700

    Added a button that allows you to turn on or off the grid

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const container = document.getElementById("container");
 
 const resetButton = document.querySelector("#reset");
+const displayGridButton = document.querySelector("#displayGrid");
+// const square = document.querySelector(".square");
 
 function createGrid(n){ // n is an integer that represents the dimensions of the grid
     document.documentElement.style.setProperty("--columns", n);
     for(let i = 0; i < n; ++i){
         for(let j = 0; j < n; ++j){
             const div = document.createElement("div");
-            div.classList.add("square")
+            div.classList.add("square");
             container.appendChild(div); 
         }
     }
@@ -19,15 +21,31 @@ function clearGrid(){
     }
 }
 
-
-resetButton.addEventListener("click", ()=>{
-    console.log("The reset button has been clicked");
-    clearGrid();
+function userInput(){
     let numberOfSquares = -1;
     while(numberOfSquares < 0 || numberOfSquares > 100){
         numberOfSquares = parseInt(prompt("What should the dimensions of the square grid be?"));
         console.log(numberOfSquares);
-        console.log("Try again. The dimensions are too small or too big.");
+        if(numberOfSquares < 0 || numberOfSquares > 100) console.log("Try again. The dimensions are too small or too big.");
     }
     createGrid(numberOfSquares);
+}
+
+resetButton.addEventListener("click", ()=>{
+    console.log("The reset button has been clicked");
+    clearGrid();
+    userInput();
 })
+
+displayGridButton.addEventListener("click", ()=>{
+    console.log("The Display Grid button has been clicked.");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square)=>{
+        square.classList.toggle("border-toggle");
+    })
+
+})
+
+// square.addEventListener("mouseover",()=>{
+//     square.classList.add("active");
+// })
